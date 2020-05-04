0de18fbd0d6e7f89b3bff078e2c997d4d7f129df
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-05-03 22:34:22 -0300

    Página de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-import SignIn from './pages/SignIn'
+// import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 import GlobalStyle from './styles/global'
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 )
