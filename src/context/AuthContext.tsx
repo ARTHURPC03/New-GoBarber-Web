cf6c8c5ff7167462acafe351fc6770f57e70a5f8
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-05-08 09:37:32 -0300

    Criando hook useAuth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback, useState } from 'react'
+import React, { createContext, useCallback, useState, useContext } from 'react'
 import api from '../services/api'
 
 interface AuthState {
@@ -16,9 +16,9 @@ interface AuthContextData {
   signIn(credentials: SignInCredentials): Promise<void>
 }
 
-export const AuthContext = createContext<AuthContextData>({} as AuthContextData)
+const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
-export const AuthProvider: React.FC = ({ children }) => {
+const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
     const token = localStorage.getItem('@GoBarber:token')
     const user = localStorage.getItem('@GoBarber:user')
@@ -49,3 +49,14 @@ export const AuthProvider: React.FC = ({ children }) => {
     </AuthContext.Provider>
   )
 }
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext)
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
+
+export { AuthProvider, useAuth }
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,10 @@
-import React, { useRef, useCallback, useContext } from 'react'
+import React, { useRef, useCallback } from 'react'
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
 
-import { AuthContext } from '../../context/AuthContext'
+import { useAuth } from '../../context/AuthContext'
 import getValidationErrors from '../../utils/getValidationErrors'
 
 import logoImg from '../../assets/logo.svg'
@@ -22,9 +22,7 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
-  const { user, signIn } = useContext(AuthContext)
-
-  console.log(user)
+  const { signIn } = useAuth()
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
