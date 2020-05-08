550a6c109d27a9a3d1c70420cf998d4a0b1f9f11
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-05-08 09:29:46 -0300

    Mantendo usuário no storage

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -22,7 +22,9 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
-  const { signIn } = useContext(AuthContext)
+  const { user, signIn } = useContext(AuthContext)
+
+  console.log(user)
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
