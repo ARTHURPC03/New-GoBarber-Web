da4b2e1083c44385359888bd917ecd5df3aa7d9b
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-07-08 05:30:39 -0300

    Próximo agendamento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
-import { FiPower } from 'react-icons/fi'
-import { Container, Header, HeaderContent, Profile } from './styles'
+import { FiPower, FiClock } from 'react-icons/fi'
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles'
 
 import logoImg from '../../assets/logo.svg'
 import { useAuth } from '../../hooks/auth'
@@ -28,6 +37,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/55156476?s=460&u=a4ce1d47e4ab2aed4888935a7ecf0948cf10002a&v=4"
+                alt="ARTHUR PC"
+              />
+              <strong>ARTHUR PC</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   )
 }
